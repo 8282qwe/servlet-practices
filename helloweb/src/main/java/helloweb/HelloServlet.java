5781076389e1ec426a80d17a6d74ba213b3e2223
Midichi <8282qwe@gmail.com>
2024-12-17 13:15:05 +0900

    Initial commit

---
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -0,0 +1,25 @@
+package helloweb;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html; charset=utf-8");
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello Servlet World</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        doGet(req, res);
+    }
+}
